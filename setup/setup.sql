45e0cc4a41aac33350f9c5a155fea6f2a618eb89
loli <ipv4sec@gmail.com>
2019-06-20 23:08:09 +0800

    修复SQL

--- setup/setup.sql
+++ setup/setup.sql
@@ -1,35 +1,33 @@
 
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE `admins` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `site_id` bigint(20) NOT NULL DEFAULT '0',
+  `site_id` int(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uix_admins_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) AUTO_INCREMENT=2;
 
 INSERT INTO `admins` VALUES ('1', 'admin', 'ab2ed29c49cd9ca21d035d7f34cd99b1', '0');
 
 DROP TABLE IF EXISTS `sites`;
 CREATE TABLE `sites` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uix_sites_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `site_id` bigint(20) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `num` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `enroll` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `major` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tag` bigint(20) NOT NULL DEFAULT '1',
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `site_id` int(12) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `num` varchar(255) NOT NULL,
+  `enroll` varchar(255) NOT NULL,
+  `major` varchar(255) NOT NULL,
+  `tag` int(12) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uix_users_num` (`num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `sites` (`id`, `name`) VALUES ('0', '总校');
+);
